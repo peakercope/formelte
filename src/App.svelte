fbba77576e6c308120720d7554c0499bae6cc929
peakercope <peakercope@gmail.com>
2021-10-02 16:37:32 +0300

    feat: Core - added field change handler.

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,22 @@
 <script>
-  import { Form, Field } from './module.js';
+  import { createForm } from './module.js';
+  const { values, handleChange, handleSubmit } = createForm({
+    defaultValues: {
+      name: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    }
+  });
+
+  console.log($values);
 </script>
 
 
-<Form>
-  <Field name="name" />
-</Form>
+<form on:submit={handleSubmit}>
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name" on:change={handleChange} bind:value={$values.name} />
+  <p>{$values.name}</p>
+
+  <button type="submit">send</button>
+</form>
--- src/module.js
+++ src/module.js
@@ -1,2 +1,3 @@
 export { default as Form } from './components/Form.svelte';
 export { default as Field } from './components/Field.svelte';
+export * from './functions/createForm.js';
